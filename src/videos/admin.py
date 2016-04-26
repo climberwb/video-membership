4e67590462f328f4d2854974c0dcf346e9b7ea3e
Warren Kushner <warren.kushner@gmail.com>
2016-04-26 16:31:30 +0000

    customized admin for slug creation

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Video, Category
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__","slug"]
+    fields=['title','embed_code',"slug",
+    "active",
+    "featured",
+    "free_preview"]
+    
+    prepopulated_fields = {"slug":["title"],
+                            "embed_code":["title"]}
+    class Meta:
+        model = Video
+        
+    
+
+admin.site.register(Video,VideoAdmin)
 admin.site.register(Category)
