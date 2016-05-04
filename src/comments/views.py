eb4a1259ea45f69d03d56d8f7b9c593392ec9a4d
Warren Kushner <warren.kushner@gmail.com>
2016-05-04 16:38:04 +0000

    added messages to comments * added success and failure messages to base.html * made the messages html safe * added bootstrap classes of warning and success

--- src/comments/views.py
+++ src/comments/views.py
@@ -23,34 +23,32 @@ def comment_thread(request,id):
 def comment_create(request):
     form = CommentForm(request.POST)
     if form.is_valid() and request.method == "POST" and request.user.is_authenticated():
-        
+        origin_path = request.POST.get('origin_path')
         # fields needed to create child comment
         parent_id = request.POST.get('parent_id')
-        print(parent_id,"parent_id")
         if parent_id:
             parent_comment = Comment.objects.get(id=parent_id)
             video = parent_comment.video
         else:
-            origin_path = request.POST.get('origin_path')
             vid_slug = origin_path.split("/")[3]
-            print(vid_slug)
             video = Video.objects.get(slug=vid_slug)
             
         comment_text = form.cleaned_data['comment']
         
         
         if(parent_id is not None):
-            new_comment = Comment.objects.create_comment(
-                                user=request.user, 
-                                path=parent_comment.get_origin,
-                                text=comment_text,
-                                parent = parent_comment,
-                                video = video)
-            if new_comment.was_saved:
-                messages.success(request,"Thank you for adding your comment")
-                messages.add_message(request, messages.INFO, 'Hello world.')
-                
+            try:
+                new_comment = Comment.objects.create_comment(
+                                    user = request.user, 
+                                    path=parent_comment.get_origin,
+                                    text=comment_text,
+                                    parent = parent_comment,
+                                    video = video)
+                messages.success(request,"Congrats your commnet was saved ")
                 return HttpResponseRedirect(new_comment.get_origin)
+            except:
+                messages.error(request,"Your comment did not save. Please try again. ")
+            return HttpResponseRedirect(origin_path)
         else:
             new_comment = Comment.objects.create_comment(
                                 user=request.user, 
