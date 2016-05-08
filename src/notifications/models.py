aa5b8b83bc8307352a7e024cdfbca66821a5a4bf
Warren Kushner <warren.kushner@gmail.com>
2016-05-08 16:49:59 +0000

    added new fields to notification

--- src/notifications/models.py
+++ src/notifications/models.py
@@ -10,11 +10,25 @@ from .signals import notify
 class Notification(models.Model):
     # sender = 
     recipient = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='notifications')
-    action = models.CharField(max_length=255)
+    #action = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
     
+    sender_content_type = models.ForeignKey(ContentType,related_name='notify_sender')
+    sender_object_id = models.PositiveIntegerField()
+    sender_object = GenericForeignKey("sender_content_type","sender_object_id")
+    
+    verb = models.CharField(max_length=255)
+    
+    action_content_type = models.ForeignKey(ContentType,related_name='notify_action',null=True,blank=True)
+    action_object_id = models.PositiveIntegerField()
+    action_object = GenericForeignKey("action_content_type","action_object_id")
+    
+    target_content_type = models.ForeignKey(ContentType,related_name='notify_target',null=True,blank=True)
+    target_object_id = models.PositiveIntegerField()
+    target_content_object = GenericForeignKey("target_content_type","target_object_id")
+    
     def __unicode__(self):
-        return str(self.action)
+        return str(self.verb)
         
 def new_notification(sender,recipient,action,*args,**kwargs):
     print(recipient)
--- src/notifications/signals.py
+++ src/notifications/signals.py
@@ -1,3 +1,3 @@
 from django.dispatch import Signal
 
-notify = Signal(providing_args=['user','action'])
+notify = Signal(providing_args=['sender','recipient','verb','action_object','target_object'])
